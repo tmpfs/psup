1a7d6e79ff7ec854d9583a61e15130ce007c03e2
muji <muji@tmpfs.org>
2021-03-03 10:05:36 +0800

    Support relaxed flag for workers.
    Closes #10.

--- impl/src/worker.rs
+++ impl/src/worker.rs
@@ -10,6 +10,7 @@ where
     F: Future<Output = Result<()>>,
 {
     handler: Option<H>,
+    relaxed: bool,
 }
 
 impl<H, F> Worker<H, F>
@@ -19,7 +20,25 @@ where
 {
     /// Create a new worker.
     pub fn new() -> Self {
-        Self { handler: None }
+        Self { handler: None, relaxed: false }
+    }
+
+    /// Set the relaxed flag.
+    ///
+    /// When a worker is relaxed it will start with or without a supervisor.
+    ///
+    /// The default is `false` so workers expect to be run in the context 
+    /// of a supervisor and it is an error if no worker id is available in 
+    /// the environment.
+    ///
+    /// When this flag is enabled and the required environment variables
+    /// do not exist the worker does not attempt to connect to a supervisor.
+    ///
+    /// Use this mode of operation when a worker process can be run standalone 
+    /// or as a worker for a supervisor process.
+    pub fn relaxed(mut self, flag: bool) -> Self {
+        self.relaxed = flag; 
+        self
     }
 
     /// Set a client connection handler.
@@ -35,16 +54,17 @@ where
     /// Start this worker running.
     pub async fn run(&self) -> Result<()> {
         if let Some(ref handler) = self.handler {
-            // Read worker information from the environment.
-            let id = std::env::var(WORKER_ID)
-                .or_else(|_| Err(Error::WorkerNoId))?
-                .to_string();
-
-            // If we were given a socket path make the connection.
-            if let Some(path) = std::env::var(SOCKET).ok() {
-                // Connect to the supervisor socket
-                let stream = UnixStream::connect(&path).await?;
-                (handler)(stream, id).await?;
+            if let Some(id) = std::env::var(WORKER_ID).ok() {
+                // If we were given a socket path make the connection.
+                if let Some(path) = std::env::var(SOCKET).ok() {
+                    // Connect to the supervisor socket
+                    let stream = UnixStream::connect(&path).await?;
+                    (handler)(stream, id.to_string()).await?;
+                }
+            } else {
+                if !self.relaxed {
+                    return Err(Error::WorkerNoId);
+                } 
             }
         }
         Ok(())
